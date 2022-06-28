b7099d109c9975bd7bbfedfee2abfbca15a9fc1b
abderahmman <108357064+abderahmman@users.noreply.github.com>
2022-06-28 09:15:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var e = document.getElementsByClassName("project-category")[1]
+e.innerHTML = "hacker"
+
+var e = document.getElementsByClassName("project-category")[2]
+e.innerHTML = "best robot"
+var e = document.getElementsByClassName("project-category")[3]
+e.innerHTML = "programing language"
